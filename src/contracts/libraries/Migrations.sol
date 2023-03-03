193cdaea7841b6851c55bb370dd59eba87f62f97
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-03-03 09:08:40 -0500

    update

---
+++ src/contracts/libraries/Migrations.sol
@@ -0,0 +1,15 @@
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Migrations {
+    address public owner = msg.sender;
+    uint public last_completed_migrations;
+
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            ""
+        );
+        _;
+    }
+}
