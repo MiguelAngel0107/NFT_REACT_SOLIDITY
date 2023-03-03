c2230b4b892a15ac631d0125ab1f15c895daf313
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-03-03 18:28:40 -0500

    Update

--- src/contracts/libraries/Migrations.sol
+++ src/contracts/libraries/Migrations.sol
@@ -8,8 +8,17 @@ contract Migrations {
     modifier restricted() {
         require(
             msg.sender == owner,
-            ""
+            "This function is restricted to the contract owner"
         );
         _;
+    }
+
+    function setCompleted(uint completed) public restricted {
+        last_completed_migrations = completed;
+    }
+
+    function upgrade(address new_address) public restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migrations);
     }
 }
